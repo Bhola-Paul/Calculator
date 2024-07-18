38a7ce91f9cdc65e259c37a9139b3ceadf3ce1eb
Bhola Paul <123001928+Bhola-Paul@users.noreply.github.com>
2024-07-18 10:46:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+let input=document.querySelector('input')
+let buttons=document.querySelectorAll('button')
+let s="";
+let arr=Array.from(buttons);
+arr.forEach(val=>{
+    val.addEventListener('click',(e)=>{
+        if(e.target.innerHTML=='='){
+            s=eval(s);
+            input.value=s;
+        }
+        else if(e.target.innerHTML=='AC'){
+            s="";
+            input.value=s;
+        }
+        else if(e.target.innerHTML=='DEL'){
+            s=s.substring(0,s.length-1);
+            input.value=s;
+        }
+        else{
+            s+=e.target.innerHTML;
+            input.value=s;
+        }
+    })
+})
